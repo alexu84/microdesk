87a07b8bb8a26799ecd722e06ade76d7d0a4c607
Alexandru Marian Vasile <codemaster64bit@gmail.com>
2026-06-10 12:44:33 +0300

    Add H2 database dependency and configuration for unit tests in booking and notification services

--- booking-service/src/test/java/com/rasirom/booking_service/BookingServiceApplicationTests.java
+++ booking-service/src/test/java/com/rasirom/booking_service/BookingServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.rasirom.booking_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 
 @SpringBootTest
+@EmbeddedKafka(partitions = 1)
 class BookingServiceApplicationTests {
 
 	@Test
